d9ba049b9899af64987cf6368227ffa21fc318e4
Mansi Varshney <mansivarshney13@gmail.com>
2022-02-17 20:18:05 +0530

    added user and messages schyma

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mansi", password: "password")
+User.create(username: "Kushaank", password: "password")
+User.create(username: "Piyush", password: "password")
+User.create(username: "Swati", password: "password")
+User.create(username: "Rajpura", password: "password")
