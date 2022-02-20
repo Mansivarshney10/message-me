ce4a583398d3d0c490a1fd35b85d3fe3fc5b4bac
Mansi Varshney <mansivarshney13@gmail.com>
2022-02-20 17:47:36 +0530

    add message from Ui

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,9 @@ class MessagesController < ApplicationController
   def create 
     message = current_user.messages.build(message_params)
     if message.save 
+      ActionCable.server.broadcast  'chatroom_channel', {foo: message.body}
       redirect_to root_path
+      return
     end
   end
 
